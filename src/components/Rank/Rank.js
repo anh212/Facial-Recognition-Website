3d2ddf8fa745ef5bf33c222725a562d1b4b0d0af
Andrew Ha <andrewha428@gmail.com>
2020-05-01 22:00:51 -0400

    Fixed displaying entries

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Rank = ({name, entries}) => {
     return (
         <div>
-            <div className='white f3'>{`${name}, your current rank is...`}</div>
-            <div className='white f1'>{`#${entries}`}</div>
+            <div className='white f3'>{`${name}, the number of images you submitted are...`}</div>
+            <div className='white f1'>{`${entries}`}</div>
         </div>
     )
 }
